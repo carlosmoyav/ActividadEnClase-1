a7013ba12579754467434f46fe4b17b1ea640ffe
carlosmoyav <56742341+carlosmoyav@users.noreply.github.com>
2019-10-21 19:29:10 -0500

    Add files via upload

---
+++ Clase.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadEnClase.clases
+{
+    public class Nota
+    {
+        static Nota() { }
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+
+        private float ac1p;
+
+        public float Ac1P
+        {
+            get
+            {
+                Console.WriteLine(ac1p);
+                Console.WriteLine(ac1p * 25);
+                Console.WriteLine(ac1p * 25 / 100);
+                return ((ac1p * 25) / 10000);
+            }
+            set
+            {
+                if (ac1p < 0 || ac1p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                ac1p = value;
+            }
+        }
+        private float pr1p;
+
+        public float Pr1P
+        {
+            get
+            {
+                return ((pr1p * 25) / 10000);
+            }
+            set
+            {
+                if (pr1p < 0 || pr1p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                pr1p = value;
+            }
+        }
+
+        private float ta1p;
+
+        public float Ta1P
+        {
+            get { return ((ta1p * 20) / 10000); }
+            set
+            {
+                if (ta1p < 0 || ta1p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                ta1p = value;
+            }
+        }
+        private float ef1p;
+
+        public float Ef1P
+        {
+            get { return ((ef1p * 30) / 10000); }
+            set
+            {
+                if (ef1p < 0 || ef1p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                ef1p = value;
+            }
+        }
+        public float PrimerParcial
+        {
+            get { return ((Ac1P + Ta1P + Ef1P + Pr1P) / 4); }
+        }
+        private float ac2p;
+        public float Ac2P
+        {
+            get { return ((ac2p * 25) / 10000); }
+            set
+            {
+                if (ac2p < 0 || ac2p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                ac2p = value;
+            }
+        }
+        private float pr2p;
+
+        public float Pr2P
+        {
+            get { return ((pr2p * 25) / 10000); }
+            set
+            {
+                if (pr2p < 0 || pr2p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                pr2p = value;
+            }
+        }
+        private float ta2p;
+
+        public float Ta2P
+        {
+            get { return ((ta2p * 20) / 10000); }
+            set
+            {
+                if (ta2p < 0 || ta2p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                ta2p = value;
+            }
+        }
+        private float ef2p;
+
+        public float Ef2P
+        {
+            get { return ((ef2p * 30) / 10000); }
+            set
+            {
+                if (ef2p < 0 || ef2p > 10)
+                {
+                    throw new Exception("El numero que ingreso no esta en el rango permitido");
+                }
+                ef2p = value;
+            }
+        }
+        public float SegundoParcial
+        {
+            get { return ((Ac2P + Ta2P + Ef2P + Ef2P) / 4); }
+        }
+        public float Suma
+        {
+            get { return ((PrimerParcial + SegundoParcial)); }
+        }
+        private float recuperacion;
+
+        public float Recuperacion
+        {
+            get { return recuperacion; }
+            set { recuperacion = value; }
+        }
+        public float Suma2
+        {
+            get { return (((Suma / 2) + Recuperacion)); }
+        }
+
+        public float Final
+        {
+            get
+            {
+                if (Suma >= 14)
+                {
+                    return (Suma / 2);
+                }
+                else
+                {
+                    return (Suma2 / 2);
+                }
+            }
+
+        }
+        public String Estado
+        {
+
+
+            get
+            {
+                if (Final >= 7)
+                {
+                    return "APROBADO";
+                }
+                else
+                {
+
+                    return "REPROBADO";
+                }
+
+
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActividadEnClase.clases;
+namespace ActividadEnClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nota nota = new Nota();
+            int ban = 0, eleccion = 0;
+
+            Console.WriteLine("Ingrese el nombre del estudiante");
+            while (ban == 0)
+            {
+                nota.Nombre = Console.ReadLine();
+                Console.WriteLine("Esta correcto su nombre?\n" + nota.Nombre + "\n" + "Ingrese 1 si desea continuar y escriba 2 si desea reescribirlo");
+                eleccion = int.Parse(Console.ReadLine());
+                if (eleccion == 1)
+                {
+                    ban = 1;
+                }
+            }
+            Console.WriteLine("Ingrese la nota de actividad en clase del primer parcial");
+            nota.Ac1P =
+            float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Ac1P);
+            Console.WriteLine("Ingrese la nota de practicas del primer parcial");
+            nota.Pr1P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Pr1P);
+            Console.WriteLine("Ingrese la nota de trabajo autonomo del primer parcial");
+            nota.Ta1P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Ta1P);
+            Console.WriteLine("Ingrese la nota de evaluacion final del primer parcial");
+            nota.Ef1P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Ef1P);
+            Console.WriteLine("Ingrese la nota de actividad en clase del segundo parcial");
+            nota.Ac2P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Ac2P);
+            Console.WriteLine("Ingrese la nota de practicas del segundo parcial");
+            nota.Pr2P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Pr2P);
+            Console.WriteLine("Ingrese la nota de trabajo autonomo del segundo parcial");
+            nota.Ta2P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Ta2P);
+            Console.WriteLine("Ingrese la nota de evaluacion final del segundo parcial");
+            nota.Ef2P = float.Parse(Console.ReadLine());
+            Console.WriteLine(nota.Ef2P);
+            if (nota.Suma < 14)
+            {
+                Console.WriteLine("Ingrese la nota de recuperacion");
+                nota.Recuperacion = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(nota.Recuperacion);
+            Console.WriteLine(nota.Suma);
+            Console.WriteLine(nota.Suma2);
+            Console.WriteLine("Su promedio es: " + nota.Final);
+            Console.WriteLine("Usted esta: " + nota.Estado);
+            Console.ReadKey();
+
+
+        }
+    }
+}
